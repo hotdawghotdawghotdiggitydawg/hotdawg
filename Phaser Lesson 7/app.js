582d9b1f6297ae108ccfe54e925a8df15ae871ba
hotdawghotdawghotdiggitydawg <djkent3388@gmail.com>
2018-02-28 18:54:44 +0800

    lesson 8

--- Phaser Lesson 7/app.js
+++ Phaser Lesson 7/app.js
@@ -45,7 +45,7 @@ function create(){
 		player.animations.add('left', [0,1,2,3],10,true);
 		player.animations.add('right', [5,6,7,8],10,true);
 		game.physics.arcade.enable(player);
-		player.body.gravity.y = 300;
+		player.body.gravity.y = 280;
 		player.body.bounce.y = 0.4;
 		player.body.collideWorldBound = true;
 
@@ -71,6 +71,14 @@ function create(){
 	//create keyboard entries
 	cursors = game.input.keyboard.createCursorKeys();
 
+	wKey = game.input.keyboard.addKey
+		(Phaser.Keyboard.W);
+	aKey = game.input.keyboard.addKey
+		(Phaser.Keyboard.W);
+	dKey = game.input.keyboard.addKey
+		(Phaser.Keyboard.W);
+
+
 }
 
 function update(){
@@ -81,11 +89,11 @@ function update(){
 	//set player velocity to 0 if now events
 	player.body.velocity.x = 0;
 
-	if(cursors.left.isDown){
+	if(cursors.left.isDown || aKey.isDown){
 		player.animations.play('left');
 		player.body.velocity.x = -150;
 
-	} else if(cursors.right.isDown){
+	} else if(cursors.right.isDown || dKey.isDown){
 		player.animations.play('right');
 		player.body.velocity.x = 150;
 	} else {
@@ -94,9 +102,13 @@ function update(){
 	}
 
 
-	if(cursors.up.isDown && player.body.touching.down){
+	if(cursors.up.isDown || wKey.isDown && player.body.touching.down){
 		player.body.velocity.y = -300
 	}
+
+		wKey.isDown;
+		aKey.isDown;
+		dKey.isDown;
 
 
 }
